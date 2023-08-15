abb0a270396dc5afb4488196a5ac1511004e30f1
Douglas Shibata <douglas.shibata11@gmail.com>
2023-08-15 16:00:27 -0300

    feat: creating class Jogo and pass atributes to html

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self,nome,categoria,console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+        
+
+
 app = Flask(__name__)
 
 @app.route('/inicio')
 def ola():
-    lista = ['Joogo 1','Jogo 2 ','Jogo 3','Joogo 4']
+    jogo1= Jogo('Tetris','Puzzle','Atarri')
+    jogo2= Jogo('God of war','Rock','PS2')
+    jogo3= Jogo ('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1,jogo2,jogo3]
     return render_template('lista.html',titulo='Jogos',jogos=lista)
 
 app.run()
