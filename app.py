0e8a15ee4087988341249f11a4e107870b45fc83
Douglas Shibata <douglas.shibata11@gmail.com>
2023-08-15 16:47:38 -0300

    feat: login e autenticação

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 class Jogo:
     def __init__(self,nome,categoria,console):
@@ -12,6 +12,7 @@ jogo3= Jogo ('Mortal Kombat', 'Luta', 'PS2')
 lista = [jogo1,jogo2,jogo3]
 
 app = Flask(__name__)
+app.secret_key = 'alura'
 
 @app.route('/')
 def index():
@@ -46,8 +47,18 @@ def criar():
     lista.append(jogo)
     return redirect('/')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + ' logou com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuário não logado.')
+        return redirect('/login')
 
-
-
-app.run(debug = True)
+app.run(debug=True)
