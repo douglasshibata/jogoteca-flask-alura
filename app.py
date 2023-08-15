35e8b56e89d68cc345aaee30d90bd2da4fd719f7
Douglas Shibata <douglas.shibata11@gmail.com>
2023-08-15 16:15:48 -0300

    feat: send form via post in html form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self,nome,categoria,console):
@@ -6,16 +6,20 @@ class Jogo:
         self.categoria=categoria
         self.console=console
         
-
+jogo1= Jogo('Tetris','Puzzle','Atarri')
+jogo2= Jogo('God of war','Rock','PS2')
+jogo3= Jogo ('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1,jogo2,jogo3]
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
 @app.route('/inicio')
 def ola():
-    jogo1= Jogo('Tetris','Puzzle','Atarri')
-    jogo2= Jogo('God of war','Rock','PS2')
-    jogo3= Jogo ('Mortal Kombat', 'Luta', 'PS2')
-    lista = [jogo1,jogo2,jogo3]
+
     return render_template('lista.html',titulo='Jogos',jogos=lista)
 
 
@@ -33,4 +37,17 @@ def apostilas_online():
 def novo():
     return render_template('novo.html', titulo='Novo Jogo')
 
-app.run()
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+
+
+
+app.run(debug = True)
