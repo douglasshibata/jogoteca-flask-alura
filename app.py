26dbd7bd5e61e656bb009511e90ef4d255b7234d
Douglas Shibata <douglas.shibata11@gmail.com>
2023-08-15 16:49:59 -0300

    feat: logout

--- app.py
+++ app.py
@@ -61,4 +61,12 @@ def autenticar():
         flash('Usuário não logado.')
         return redirect('/login')
 
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect('/')
+
+
 app.run(debug=True)
